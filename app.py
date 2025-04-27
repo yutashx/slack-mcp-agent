23a498aa98e6c4929f170f2f8f57c29f6cbbf1d6
yutashx <yutashx@gmail.com>
2025-04-27 13:37:13 +0900

    support openai agent sdk

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ from mcp_use import MCPClient, MCPAgent
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from utils import format_slack_event, agent_behavior_prompt
+import json
+import datetime
+from agents import Agent, Runner, function_tool, WebSearchTool
+from agents.mcp import MCPServerStdio
+import contextlib, io
 
 # .env ファイルから環境変数を読み込む
 load_dotenv()
@@ -20,47 +25,53 @@ AGENT_MAX_STEP = int(os.getenv("AGENT_MAX_STEP", 15))
 
 # 初期化
 app = AsyncApp(token=SLACK_BOT_TOKEN)
-client = MCPClient.from_config_file(MCP_SERVER_CONFIG_PATH)
-llm = ChatOpenAI(
-    model=LLM_MODEL,
-)
+with open(MCP_SERVER_CONFIG_PATH, "r") as f:
+    mcp_servers = json.load(f)["mcpServers"]
 
-# session cache
-session_cache = {}
+@function_tool
+async def clock() -> str:
+    """Get current time in JST."""
+    t = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return t
+
+@function_tool
+async def get_str_lenth(text: str) -> int:
+    """Get the length of a string."""
+    return len(text)
 
 # メンションイベントのハンドリング
 @app.event("app_mention")
 async def handle_app_mention(event, say):
-    print(f"Received mention from {event['user']} in channel {event['channel']}")
     print(f"Event: {event}") 
-
-    agent = None
-    thread_ts = event.get("thread_ts", event["ts"])
-    # セッションキャッシュの確認
-    if thread_ts in session_cache:
-        agent = session_cache[thread_ts]
-    else:
-        agent = MCPAgent(
-            llm=llm,
-            client=client,
-            max_steps=AGENT_MAX_STEP,
-            memory_enabled=True,
-            verbose=True,
-            use_server_manager=True,
-            system_prompt=agent_behavior_prompt(os.getenv("SLACK_USER_ID")),
+    async with MCPServerStdio(params=mcp_servers["slack"] , cache_tools_list=True) as slack, \
+               MCPServerStdio(params=mcp_servers["notion"], cache_tools_list=True) as notion, \
+               MCPServerStdio(params=mcp_servers["playwright"], cache_tools_list=True) as playwright:
+        instructions = agent_behavior_prompt(os.getenv("SLACK_USER_ID"))
+        agent = Agent(
+            name="Tachikoma Assistant",
+            instructions=instructions,
+            mcp_servers=[
+                slack,
+                notion,
+            ],
+            tools=[
+                clock,
+                get_str_lenth,
+                WebSearchTool()
+            ],
+            model="gpt-4o-mini",
         )
-        session_cache[thread_ts] = agent
 
-    try:
-        # Run the agent with the user input (memory handling is automatic)
-        response = await agent.run(format_slack_event(event))
-        print(response)
-    except Exception as e:
-        print(f"\nError: {e}")
-
-    # 結果を返信
-    await say(text=response, thread_ts=thread_ts)
-    print(f"Replied to {event['user']} in channel {event['channel']}: {response}")
+        try:
+            result = await Runner.run(
+                agent,
+                input=format_slack_event(event),
+            )
+            if result.final_output:
+                print(f"Sending reply to {event['channel']} thread {event['ts']}: {result.final_output}")
+                await say(text=result.final_output, thread_ts=event["ts"])
+        except Exception as e:
+                await say(text=str(e), thread_ts=event["ts"])
 
 # アプリの起動
 async def main():
