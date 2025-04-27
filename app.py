5fe9fb105a3634d382d3e4f3212331838560ef24
yutashx <yutashx@gmail.com>
2025-04-27 12:22:43 +0900

    try mcp-use, but cannot agent loop

--- app.py
+++ app.py
@@ -21,7 +21,9 @@ AGENT_MAX_STEP = int(os.getenv("AGENT_MAX_STEP", 15))
 # 初期化
 app = AsyncApp(token=SLACK_BOT_TOKEN)
 client = MCPClient.from_config_file(MCP_SERVER_CONFIG_PATH)
-llm = ChatOpenAI(model=LLM_MODEL)
+llm = ChatOpenAI(
+    model=LLM_MODEL,
+)
 
 # session cache
 session_cache = {}
@@ -29,14 +31,11 @@ session_cache = {}
 # メンションイベントのハンドリング
 @app.event("app_mention")
 async def handle_app_mention(event, say):
-    user_id = event["user"]
-    text = event["text"]
-    channel = event["channel"]
-    thread_ts = event.get("thread_ts", event["ts"])
-    print(f"Received mention from {user_id} in channel {channel}: {text}")
-    print(event)
+    print(f"Received mention from {event['user']} in channel {event['channel']}")
+    print(f"Event: {event}") 
 
     agent = None
+    thread_ts = event.get("thread_ts", event["ts"])
     # セッションキャッシュの確認
     if thread_ts in session_cache:
         agent = session_cache[thread_ts]
@@ -47,26 +46,26 @@ async def handle_app_mention(event, say):
             max_steps=AGENT_MAX_STEP,
             memory_enabled=True,
             verbose=True,
+            use_server_manager=True,
+            system_prompt=agent_behavior_prompt(os.getenv("SLACK_USER_ID")),
         )
         session_cache[thread_ts] = agent
 
     try:
         # Run the agent with the user input (memory handling is automatic)
-        agent_prompt = agent_behavior_prompt() + "\n" + format_slack_event(event)
-        response = await agent.run(agent_prompt)
+        response = await agent.run(format_slack_event(event))
         print(response)
     except Exception as e:
         print(f"\nError: {e}")
 
     # 結果を返信
     await say(text=response, thread_ts=thread_ts)
-    print(f"Replied to {user_id} in channel {channel}: {response}")
+    print(f"Replied to {event['user']} in channel {event['channel']}: {response}")
 
 # アプリの起動
 async def main():
     handler = AsyncSocketModeHandler(app, SLACK_APP_TOKEN)
     await handler.start_async()
-
 
 
 if __name__ == "__main__":
