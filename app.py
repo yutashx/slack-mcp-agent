97a83b25c61e42544ba5e99fb59507a013bfa226
yutashx <yutashx@gmail.com>
2025-04-27 16:25:08 +0900

    設定項目を全てconfig.jsonにまとめた

--- app.py
+++ app.py
@@ -2,31 +2,22 @@ import os
 import asyncio
 from slack_bolt.async_app import AsyncApp
 from slack_bolt.adapter.socket_mode.aiohttp import AsyncSocketModeHandler
-from mcp_use import MCPClient, MCPAgent
-from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
 from utils import format_slack_event, agent_behavior_prompt
 import json
 import datetime
-from agents import Agent, Runner, function_tool, WebSearchTool
+from agents import Agent, Runner, function_tool
 from agents.mcp import MCPServerStdio
-import contextlib, io
 
-# .env ファイルから環境変数を読み込む
-load_dotenv()
-
-# 環境変数の取得
-SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
-SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-LLM_MODEL = os.getenv("LLM_MODEL", "gpt-4o-mini")
-MCP_SERVER_CONFIG_PATH = os.getenv("MCP_SERVER_CONFIG_PATH")
-AGENT_MAX_STEP = int(os.getenv("AGENT_MAX_STEP", 15))
+AGENT_MAX_STEP = 15
 
 # 初期化
-app = AsyncApp(token=SLACK_BOT_TOKEN)
-with open(MCP_SERVER_CONFIG_PATH, "r") as f:
-    mcp_servers = json.load(f)["mcpServers"]
+with open("./config.json", "r") as f:
+    config = json.load(f)
+
+mcp_servers = config["mcpServers"]
+env = config["env"]
+
+app = AsyncApp(token=env["SLACK_BOT_TOKEN"])
 
 @function_tool
 async def clock() -> str:
@@ -56,12 +47,10 @@ async def read_stderr() -> str:
 # メンションイベントのハンドリング
 @app.event("app_mention")
 async def handle_app_mention(event, say):
-    print(f"Event: {event}") 
-
     async with MCPServerStdio(params=mcp_servers["slack"] , cache_tools_list=True, client_session_timeout_seconds=15) as slack, \
                MCPServerStdio(params=mcp_servers["notion"], cache_tools_list=True, client_session_timeout_seconds=15) as notion, \
                MCPServerStdio(params=mcp_servers["perplexity-ask"], cache_tools_list=True, client_session_timeout_seconds=30) as perplexity:
-        instructions = agent_behavior_prompt(os.getenv("SLACK_USER_ID"))
+        instructions = agent_behavior_prompt(env["SLACK_USER_ID"])
         agent = Agent(
             name="Tachikoma Assistant",
             instructions=instructions,
@@ -76,7 +65,7 @@ async def handle_app_mention(event, say):
                 read_stdout,
                 read_stderr,
             ],
-            model="gpt-4o-mini",
+            model=env["LLM_MODEL"],
         )
 
         try:
@@ -91,7 +80,7 @@ async def handle_app_mention(event, say):
 
 # アプリの起動
 async def main():
-    handler = AsyncSocketModeHandler(app, SLACK_APP_TOKEN)
+    handler = AsyncSocketModeHandler(app, env["SLACK_APP_TOKEN"])
     await handler.start_async()
 
 
